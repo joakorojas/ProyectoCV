c12747b1dc77922f8073444e2afd26093e9e6d5f
Joaquin Rojas <89490@sistemas.frc.utn.edu.ar>
2024-02-13 17:08:28 -0300

    agrego feature login + validación básica usuario

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Proyecto personal donde crearé una landing page que funcione como curriculum 
 Tendrá varias secciones donde se mostraran mis capacidades y conocimientos
 Inicio 24/01/24
+
+########  login    #####
+user: admin
+password: admin 
+########################
---
+++ js/login.js
@@ -0,0 +1,24 @@
+
+
+
+
+const login = () => {
+    let user = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+
+    let form = document.getElementById('formLogin')
+
+    //console.log(`El usuario es ${user} con contraseña ${password}`)
+
+    let auth_user = 'admin';
+    let auth_password = 'admin';
+    
+    if (user == auth_user & password == auth_password){
+        location.href = 'index.html'
+    }
+    else{
+        alert('Usuario y/o contraseña incorrectos')
+    }
+    form.reset();
+    
+}
